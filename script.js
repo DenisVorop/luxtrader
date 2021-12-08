12e93a1bf563215f89c342f176bd07b407321013
DenisVorop <95316053+DenisVorop@users.noreply.github.com>
2021-12-09 02:21:58 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ let iconMenu = document.querySelector(".icon-menu");
 if (iconMenu != null) {
     let menuBody = document.querySelector(".menu__body");
     iconMenu.addEventListener('click', function(e) {
+        document.body.classList.toggle('_lock')
         iconMenu.classList.toggle("_active");
         menuBody.classList.toggle("_active");
     });
