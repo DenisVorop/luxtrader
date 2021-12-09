39366ac6c8bcda944feb9ac77c29955d8bb5dd55
DenisVorop <95316053+DenisVorop@users.noreply.github.com>
2021-12-09 04:02:38 +0300

    Add files via upload

--- script.js
+++ script.js
@@ -25,53 +25,28 @@ document.addEventListener('click', function(e) {
 });
 // ------------------SLIDER------------------
 
-let sliders = document.querySelectorAll('_swiper');
-if (sliders) {
-    for (let index = 0; index < sliders.length; index++) {
-        let slider = sliders[index];
-        if (!slider.classList.contains('swiper-bild')) {
-            let slider_items = slider.children;
-            if (slider_items) {
-                for (let index = 0; index < slider_items.length; index++) {
-                    let el = slider_items[index];
-                    el.classList.add('swiper-slide');
-                }
-            }
-            let slider_content = slider.innerHTML;
-            let slider_wrapper = document.createElement('div');
-            slider_wrapper.classList.add('swiper-wrapper');
-            slider_wrapper.innerHTML = slider_content;
-            slider.innerHTML = '';
-            slider.appendChild(slider_wrapper);
-            slider.classList.add('swiper-bild');
-        }
-        if (slider.classList.contains('_gallery')) {
-            //slider.data(''lightGallery).destroy(true);
-        }
-    }
-    sliders_bild_callback();
-}
-
-function sliders_bild_callback(params) { }
-
-let main_slider = new Swiper('.main-slider__body', {
-
-    observer: true,
-    observeParents: true,
-    slidesPerView: 1,
-    spaceBetween: 0,
-    //autoHeight: true,
-    speed: 800,
-    //touchRatio: 0,
-    //simulateTouch: false,
-    loop: true,
-    //preloadImages: false,
-    //lazy: true,
-
-    //arrows
+new Swiper('.main-slider__body', {
     navigation: {
-        nextE1: 'about__more .more__item_next',
-        prevE1: 'about__more .more__item_prev'
-    }
+        nextEl: '.control-main-slider__arrow_next',
+        prevEl: '.control-main-slider__arrow_prev'
+    },
 
-})
+    simulateTouch: false,
+
+    autoHeight: true,
+
+    loop: true,
+
+    // autoplay: {
+    //     delay: 5000,
+    //     stopOnLastSlide: false,
+    //     disableOnInteraction: false
+    // },
+
+    speed: 800,
+
+    // effect: 'fade',
+    // fadeEffect: {
+    //     crossFade: true
+    // },
+});
